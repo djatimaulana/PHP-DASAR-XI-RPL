4497482ab02d4ec0de46fdcd42548b35da4ab821
djatimaulana <djatimaulanahardian703@gmail.com>
2025-09-15 13:42:41 +0700

    variabel

---
+++ variabel/latihan2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>variabel latihan 2</title>
+</head>
+<body>
+<?php
+	$nama="Djati";
+	$kelas="11 RPL";
+	$alamat="jl otista gang merak no 9 subang kota";
+
+	echo "Nama : $nama";
+	echo "<br>";
+	echo "kelas : $kelas";
+	echo"<br>";
+	echo "Alamat : $alamat";
+?>
+</body>
+</html>
